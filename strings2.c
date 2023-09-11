3336fd47b2cd6d3d5f82b9aff747948f1d068afa
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 08:57:49 -0700

    more string functions

--- strings2.c
+++ strings2.c
@@ -91,3 +91,28 @@ char *_strcpy(char *dest, const char *src)
 
 	return (dest);
 }
+/**
+ * _strncpy - Copies at most @n number of bytes
+ *            from string @src into @dest.
+ *
+ * @dest: a pointer to the buffer storing the string copy.
+ * @src: a pointer to the source string.
+ * @n: The maximum number of bytes to be copied from @src.
+ *
+ * Return: a pointer to the resulting string @dest.
+ */
+char *_strncpy(char *dest, const char *src, int n)
+{
+	int i = 0, src_length = 0;
+
+	while (src[i++] != '\0')
+		src_length++;
+
+	for (i = 0; (src[i] != '\0') && (i < n); i++)
+		dest[i] = src[i];
+
+	for (i = src_length; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
