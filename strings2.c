b77c9b8da914f731ea7d5f32b8ef356324eba0f9
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 08:56:53 -0700

    more string functions

--- strings2.c
+++ strings2.c
@@ -69,3 +69,25 @@ char *_strstr(char *haystack, char *needle)
 
 	return ('\0');
 }
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating
+ *			 null byte (\0), to the buffer pointed to by dest.
+ *
+ * @src: pointer to the source string
+ * @dest: pointer to the destination string
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int i = -1;
+
+	while (src[++i] != '\0')
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
