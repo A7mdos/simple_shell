3fbed26bea600d6a520a0ec73bb5c404114570e1
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 08:46:43 -0700

    more string functions

--- strings1.c
+++ strings1.c
@@ -24,3 +24,24 @@ int _strlen(const char *s)
 
 	return (length);
 }
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: the string to be concatenated upon.
+ * @src: the source string to be appended to @dest.
+ *
+ * Return: a pointer to the resulting string @dest.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	int i = 0, j, dest_length = 0;
+
+	while (dest[i++] != '\0')
+		dest_length++;
+
+	i = dest_length;
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i++] = src[j];
+
+	return (dest);
+}
