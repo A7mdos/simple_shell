94ad8c42cafe0dd2b69838715998010b62d0053e
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 08:56:15 -0700

    more string functions

--- strings2.c
+++ strings2.c
@@ -34,3 +34,38 @@ char *_strchr(char *s, char c)
 
 	return ('\0');
 }
+/**
+ * _strstr - Locates a substring.
+ *
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: If the substring is located, a pointer to the beginning
+ *                                       of the located substring.
+ *         If the substring is not located, NULL.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+
+			j++;
+		}
+
+		if (needle[j] == '\0')
+			return (haystack + i);
+
+		i++;
+	}
+
+	return ('\0');
+}
