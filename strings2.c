410c49af5343a3398ad4bf9b856f8a31d1915d7c
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 08:55:33 -0700

    more string functions

---
+++ strings2.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/* strdup() is not allowed - Implement ur own */
+
+char *_strchr(char *s, char c);
+char *_strstr(char *haystack, char *needle);
+char *_strcpy(char *dest, const char *src);
+char *_strncpy(char *dest, const char *src, int n);
+char *_strdup(char *str);
+
+/**
+ * _strchr - Locates a character in a string.
+ *
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: If @c is found, a pointer to the first occurence.
+ *         If @c is not found, NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+
+		i++;
+	}
+
+	if (s[i] == c)
+		return (s + i);
+
+	return ('\0');
+}
