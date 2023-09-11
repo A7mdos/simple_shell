17b77eb5a53d3f36b2b30163950822e5f2dd2caf
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 08:58:23 -0700

    more string functions

--- strings2.c
+++ strings2.c
@@ -116,3 +116,32 @@ char *_strncpy(char *dest, const char *src, int n)
 
 	return (dest);
 }
+/**
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ *
+ * @str: The string to be copied.
+ *
+ * Return: If str == NULL or you are poor (insufficient RAM) - NULL.
+ *         Otherwise - a pointer to the duplicated string.
+ */
+char *_strdup(char *str)
+{
+	unsigned int str_length, i;
+	char *copy;
+
+	if (str == NULL)
+		return (NULL);
+
+	str_length = _strlen(str);
+	copy = (char *)malloc(sizeof(char) * (str_length + 1));
+
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < str_length; i++)
+		copy[i] = str[i];
+	copy[str_length] = '\0';
+
+	return (copy);
+}
