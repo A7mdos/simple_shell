345c55d049db97663e4b6116bec2cd7819b7a3a5
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 09:06:52 -0700

    morw environment functions

--- environment.c
+++ environment.c
@@ -27,3 +27,73 @@ char *_getenv(const char *name)
 	}
 	return (NULL);
 }
+/**
+ * _setenv - Changes or Adds an environment variable.
+ *
+ * @name: The name of the environment variable to change or add.
+ * @value: The value of the environment variable to change or add.
+ * @overwrite: A flag indicating if the variable should be overwritten.
+ *
+ * Return: If you are poor (insufficient RAM) - -1.
+ *         Otherwise - 0.
+ */
+int _setenv(const char *name, const char *value, int overwrite)
+{
+	char *env_var, *new_value;
+	char **new_environ;
+	size_t envsize = 0;
+	int i;
+	size_t name_len = _strlen(name),
+		   value_len = _strlen(value);
+
+	env_var = _getenv(name);
+
+	new_value = malloc(sizeof(char) * (name_len + 1 + value_len + 1));
+	if (!new_value)
+		return (-1);
+
+	_strcpy(new_value, name);
+	_strcat(new_value, "=");
+	_strcat(new_value, value);
+
+	while (environ[envsize])
+		envsize++;
+
+	if (env_var)
+		new_environ = malloc(sizeof(char *) * (envsize + 1));
+	else
+		new_environ = malloc(sizeof(char *) * (envsize + 2));
+	if (!new_environ)
+	{
+		free(new_value);
+		return (-1);
+	}
+
+	for (i = 0; environ[i]; i++)
+	{
+		new_environ[i] = malloc(_strlen(environ[i] + 1));
+		if (!new_environ[i])
+		{
+			for (i--; i >= 0; i--)
+				free(new_environ[i]);
+			free(new_environ);
+			free(new_value);
+			return (-1);
+		}
+		_strcpy(new_environ[i], environ[i]);
+	}
+
+	environ = new_environ;
+	env_var = _getenv(name);
+
+	if (env_var && overwrite)
+	{
+		env_var = new_value;
+		return (0);
+	}
+
+	environ[i] = new_value;
+	environ[i + 1] = NULL;
+
+	return (0);
+}
