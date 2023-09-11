8f3ab03a8e375c47dce0cff185980506c478f8e0
monatagelsir7 <monatagelsir777@gmail.com>
2023-09-11 08:47:26 -0700

    more string functions

--- strings1.c
+++ strings1.c
@@ -45,3 +45,25 @@ char *_strcat(char *dest, const char *src)
 
 	return (dest);
 }
+/**
+ * _strncat - concatenates two strings using at most n bytes from @src
+ *
+ * @dest: the string to be concatenated upon.
+ * @src: the string to be appended to @dest.
+ * @n: The number of bytes from src to be appended to @dest.
+ *
+ * Return: the resulting string @dest.
+ */
+char *_strncat(char *dest, const char *src, int n)
+{
+	int i = 0, j, dest_length = 0;
+
+	while (dest[i++] != '\0')
+		dest_length++;
+
+	i = dest_length;
+	for (j = 0; (src[j] != '\0') && (j < n); j++)
+		dest[i++] = src[j];
+
+	return (dest);
+}
